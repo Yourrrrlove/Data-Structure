5e2b1857a28b4970700337d4decb927cff266585
Yourrrrlove <98163348+Yourrrrlove@users.noreply.github.com>
2023-09-25 19:30:16 +0800

    Create 串的BF算法.cpp

---
+++ 串的BF算法.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int test(string &a, string &b)
+{
+	int i = 0;
+	int j = 0;
+	while (i < a.size() && j < b.size())
+	{
+		if (a[i] == b[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			i = i - j + 2;
+			j = 1;
+		}
+	}
+	if (j >= b.size() - 1)
+	{
+		return i - b.size();
+	}
+	else
+		return 0;
+}
+int main()
+{
+	string a = "ccaaabaab";
+	string b = "aaab";
+	cout << test(a, b) << endl;
+	system("pause");
+	return 0;
+}
