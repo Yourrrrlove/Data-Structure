7d91c6585d2daf66810e013ce82a8b1bb71a429a
Yourrrrlove <98163348+Yourrrrlove@users.noreply.github.com>
2023-09-26 10:41:56 +0800

    Create BF算法foe循环.cpp

---
+++ BF算法foe循环.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int test(string &a, string &b)
+{
+	int i = 0;
+	int j = 0;
+	while (i < a.size() && j < b.size())
+	{
+		if (a[i] == b[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			i = i - j + 1;
+			j = 0;
+		}
+	}
+	if (j >= b.size())
+	{
+		return i - b.size();
+	}
+	else
+		return 0;
+}
+int test2(string& a, string& b)
+{
+	int i;
+	int j;
+	for (i = 0; i < a.size(); )
+	{
+		for (j = 0; j < b.size();)
+		{
+			if (a[i] == b[j])
+			{
+				i++;
+				j++;
+			}
+			else
+			{
+				i = i - j + 1;
+				j = 0;
+			}
+		}
+	}
+	if (j >= b.size()-1)
+	{
+		return i - b.size();
+	}
+	else
+		return 0;
+}
+int main()
+{
+	string a = "abccaaabaab";
+	string b = "baab";
+	cout << test2(a, b) << endl;
+	system("pause");
+	return 0;
+}
